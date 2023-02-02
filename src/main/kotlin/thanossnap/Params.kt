8a2af1bf5b1bd970de391368fd37f375800c4ce2
Vamsi <vamsik.paladugu@gmail.com>
2023-02-02 15:19:25 +0530

    When Thanos snap

---
+++ src/main/kotlin/thanossnap/Params.kt
@@ -0,0 +1,8 @@
+package thanossnap
+
+const val SpaceX = 1000
+const val maleSpeciesPerHundred = 50
+const val maxChildrenPerSpecies = 4
+const val probabilityOfFallInLove = 25 // times out of 100
+const val maxLifeSpan = 400
+const val preAdultAge = 200
